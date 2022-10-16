c33fd628a83e98b063210f2384be99f846f5bc84
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-17 01:11:45 +0300

    styling

---
+++ frontend/src/UserSignupPage.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import axios from 'axios';
+
+class UserSignupPage extends React.Component{
+
+    state = {
+        username:null,
+        displayName:null,
+        password:null,
+        passwordRepeat:null
+    }
+
+    onChange = event => {
+
+        // const value = event.target.value;
+        // const name = event.target.name;
+        const{name,value} = event.target        
+        this.setState({
+            [name]:value
+        })
+
+    }
+
+    // onChangeUsername = event =>  {
+    //     this.setState({
+    //         username:event.target.value
+    //     });
+    //  }
+
+    //  onChangeDisplayName = event =>  {
+    //     this.setState({
+    //         displayName:event.target.value
+    //     });
+    //  }
+
+    //  onChangePassword = event =>  {
+    //     this.setState({
+    //         password:event.target.value
+    //     });
+    //  }
+
+    //  onChangePasswordRepeat = event =>  {
+    //     this.setState({
+    //         passwordRepeat:event.target.value
+    //     });
+    //  }
+    onClickSignup = event => {
+        event.preventDefault();
+        const{username,displayName,password} = this.state;
+        const body = {
+            username,
+            displayName,
+            password
+        }
+
+        axios.post('/api/1.0/users',body)
+    }
+
+    
+
+    render(){
+        return (
+            <div className = "container">
+                <form>
+                 <h1 className = "text-center">SignUp</h1>
+                 <div className="form-group">
+                 <label>Username</label>
+                 <input  className="form-control" name ="username" onChange={this.onChange}/>
+                 </div>
+                 <div className="form-group">
+                 <label>Display Name</label>
+                 <input className="form-control" name ="displayName" onChange={this.onChange}/>
+                 </div>
+                 <div className="form-group">
+                 <label>Password </label>
+                 <input  className="form-control" name ="password" onChange={this.onChange} type="password"/>
+                 </div>
+                 <div className="form-group">
+                 <label>Password Repeat </label>
+                 <input  className="form-control" name ="passwordRepeat" onChange={this.onChange} type="password"/>
+                 </div>
+                 <div className='text-center'>
+                 <button className='btn btn-primary' onClick={this.onClickSignup}>Sign up</button>
+
+                 </div>
+                 
+            </form>
+            </div>
+            
+           
+        );
+    }
+}
+
+export default UserSignupPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './bootstrap-override.scss';
+
+import UserSignupPage from './UserSignupPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserSignupPage />
   </React.StrictMode>
 );
 
