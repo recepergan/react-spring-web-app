4836b16e6160ef0874c2125322f79c2efb92d21f
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-24 11:09:07 +0300

    i18n-on-frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './bootstrap-override.scss';
+import './i18n';
 
 import UserSignupPage from './pages/UserSignupPage';
 import reportWebVitals from './reportWebVitals';
--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -1,102 +1,84 @@
 import React from 'react';
-import {signup} from '../api/apiCalls';
+import { signup } from '../api/apiCalls';
 import Input from '../components/Input';
+import { withTranslation } from 'react-i18next';
 
-class UserSignupPage extends React.Component{
+class UserSignupPage extends React.Component {
+  state = {
+    username: null,
+    displayName: null,
+    password: null,
+    passwordRepeat: null,
+    pendingApiCall: false,
+    errors: {}
+  };
 
-    state = {
-        username:null,
-        displayName:null,
-        password:null,
-        passwordRepeat:null,
-        pendingApiCall:false,
-        errors: {}
-        };
-    
-
-    onChange = event => {
-
-        // const value = event.target.value;
-        // const name = event.target.name;
-        const{name,value} = event.target 
-        const errors = {...this.state.errors}
-        errors[name] = undefined;
-        if(name === 'password' || name === 'passwordRepeat') {
-            if(name === 'password' && value !== this.state.passwordRepeat) {
-                errors.passwordRepeat = 'Password mismatch'
-            }else if(name === 'passwordRepeat' && value !== this.state.password) {
-                errors.passwordRepeat = 'Password mismatch'
-            }else{
-                errors.passwordRepeat= undefined;
-            }
-        }      
-        this.setState({
-            [name]:value,
-            errors
-        });
-
-    };
-
-    
-
-    onClickSignup = async event => {
-        event.preventDefault();
-        const{username,displayName,password} = this.state;
-        const body = {
-            username,
-            displayName,
-            password
-        };
-        this.setState({pendingApiCall:true})        
-        try {
-            const response = await signup(body); //await kodumuzun response beklemesini sağlar
-
-        }catch(error){
-            if(error.response.data.validationErrors){
-                this.setState({errors:error.response.data.validationErrors});
-
-            }
-        
-        }
-        this.setState({pendingApiCall: false})
-    };
-
-
-    
-
-    
-
-    render(){
-        const { pendingApiCall, errors} = this.state;
-        const { username,displayName,password,passwordRepeat} = errors;
-        return (
-            <div className = "container">
-                <form>
-                 <h1 className = "text-center">SignUp</h1>
-                 <Input name="username" label="Username" error={username} onChange={this.onChange}></Input>
-                 <Input name="displayName" label="Display Name" error={displayName} onChange={this.onChange}></Input>
-                 <Input name="password" label="Password" error={password} onChange={this.onChange} type="password"></Input>
-                 <Input name ="passwordRepeat" label="Password Repeat" error = {passwordRepeat} onChange={this.onChange} type="password"></Input>
-                        
-                 
-             
-                 <div className='text-center'>
-                 <button className='btn btn-primary' 
-                 onClick={this.onClickSignup}
-                 disabled={pendingApiCall || passwordRepeat !== undefined}
-                 >Sign up
-                 {pendingApiCall &&  <span className="spinner-border spinner-border-sm"></span>};
-                 
-                 </button>
-
-                 </div>
-                 
-            </form>
-            </div>
-            
-           
-        );
+  onChange = event => {
+    const { t } = this.props;
+    const { name, value } = event.target;
+    const errors = { ...this.state.errors };
+    errors[name] = undefined;
+    if (name === 'password' || name === 'passwordRepeat') {
+      if (name === 'password' && value !== this.state.passwordRepeat) {
+        errors.passwordRepeat = t('Password mismatch');
+      } else if (name === 'passwordRepeat' && value !== this.state.password) {
+        errors.passwordRepeat = t('Password mismatch');
+      } else {
+        errors.passwordRepeat = undefined;
+      }
     }
+    this.setState({
+      [name]: value,
+      errors
+    });
+  };
+
+  onClickSignup = async event => {
+    event.preventDefault();
+
+    const { username, displayName, password } = this.state;
+
+    const body = {
+      username,
+      displayName,
+      password
+    };
+    this.setState({ pendingApiCall: true });
+
+    try {
+      const response = await signup(body);
+    } catch (error) {
+      if (error.response.data.validationErrors) {
+        this.setState({ errors: error.response.data.validationErrors });
+      }
     }
 
-export default UserSignupPage;
+    this.setState({ pendingApiCall: false });
+  };
+
+  render() {
+    const { pendingApiCall, errors } = this.state;
+    const { username, displayName, password, passwordRepeat } = errors;
+    const { t } = this.props;
+    return (
+      <div className="container">
+        <form>
+          <h1 className="text-center">{t('Sign Up')}</h1>
+          <Input name="username" label={t('Username')} error={username} onChange={this.onChange} />
+          <Input name="displayName" label={t('Display Name')} error={displayName} onChange={this.onChange} />
+          <Input name="password" label={t('Password')} error={password} onChange={this.onChange} type="password" />
+          <Input name="passwordRepeat" label={t('Password Repeat')} error={passwordRepeat} onChange={this.onChange} type="password" />
+          <div className="text-center">
+            <button className="btn btn-primary" onClick={this.onClickSignup} disabled={pendingApiCall || passwordRepeat !== undefined}>
+              {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>} {t('Sign Up')}
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const UserSignupPageWithTranslation = withTranslation()(UserSignupPage);
+
+export default UserSignupPageWithTranslation;
