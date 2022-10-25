507559d89a1d81775907899b8809f01b42f7cda9
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-25 13:39:43 +0300

    authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ import LanguageSelector from './components/LanguageSelector';
 
 ReactDOM.render(
   <div>
-    <UserSignupPage />
+    <LoginPage />
     <LanguageSelector />
   </div>,
   document.getElementById('root')
